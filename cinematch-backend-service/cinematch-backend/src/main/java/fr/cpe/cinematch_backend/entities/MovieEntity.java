1ff810f2b02bea0b1e59064b47e84015c950e128
T-H-O-M-A-S <marin.f.thomas@gmail.com>
2025-04-10 16:31:12 +0200

    feat: movieEntity & movie CRUD endpoint

---
+++ cinematch-backend-service/cinematch-backend/src/main/java/fr/cpe/cinematch_backend/entities/MovieEntity.java
@@ -0,0 +1,30 @@
+package fr.cpe.cinematch_backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "movie")
+public class MovieEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    private Date releaseDate;
+
+    private String poster;
+
+}
