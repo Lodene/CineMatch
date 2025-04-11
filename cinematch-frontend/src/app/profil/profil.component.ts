cbd22880eb1ea6bbe2acb10b0a5d01355783b49e
OumaimaRTAYLI <oumaima.rtayli@gmail.com>
2025-04-11 12:30:19 +0200

    supp film + correction

--- cinematch-frontend/src/app/profil/profil.component.ts
+++ cinematch-frontend/src/app/profil/profil.component.ts
@@ -23,14 +23,15 @@ import { FeaturedFilmComponent } from '../featured-film/featured-film.component'
     MatCardModule,       
     MatDividerModule,    
     MatButtonModule,     
-    TranslatePipe,
-    FeaturedFilmComponent
+    TranslatePipe
   ],  
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.scss']
 
 })
 export class ProfilComponent {
+  favoriteMovies: Movie[] = [];
+
   userProfile: User = {
     id: 1,
     name: 'John Doe',
@@ -42,26 +43,10 @@ export class ProfilComponent {
     favoriteGenres: ['Drama', 'Action']
   }
   
-  favoriteMovies: Movie[] = [
-    {
-      id: 1,
-      title: 'The Shawshank Redemption',
-      description: 'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
-      releaseDate: 1994,
-      poster: 'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
-      genre: ['Drama', 'Crime'],
-      rating: 9.99,
-      duration: 142
-    },
-    {
-      id: 2,
-      title: 'Inception',
-      description: 'A thief who steals corporate secrets through dream-sharing technology...',
-      releaseDate: 2010,
-      poster: 'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
-      genre: ['Sci-Fi', 'Action'],
-      rating: 8.8,
-      duration: 148
-    }
-  ];
+  ngOnInit(): void {
+    this.favoriteMovies.push(new Movie());
+    this.favoriteMovies.push(new Movie());
+    this.favoriteMovies.push(new Movie());
+    
+  }
 }
