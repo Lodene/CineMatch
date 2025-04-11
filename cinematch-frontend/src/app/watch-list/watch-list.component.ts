0327a90c51c5cf55cf275b3e8cd9016040bb3479
raghad-2000 <raghad.alhomsi2225@gmail.com>
2025-04-11 09:53:18 +0200

    Oups

--- cinematch-frontend/src/app/watch-list/watch-list.component.ts
+++ cinematch-frontend/src/app/watch-list/watch-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MovieCardComponentHorizontal } from "../common-component/movie-card-horizontal/movie-card-horizontal.component";
 import { Movie } from '../../models/movie';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-watch-list',
-  imports: [MovieCardComponentHorizontal],
+  imports: [MovieCardComponentHorizontal, NgFor],
   templateUrl: './watch-list.component.html',
   styleUrl: './watch-list.component.scss'
 })
