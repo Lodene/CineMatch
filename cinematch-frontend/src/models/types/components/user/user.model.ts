59eac9f09531c70cf7cec7bd670ba808b6397d4b
OumaimaRTAYLI <oumaima.rtayli@gmail.com>
2025-04-16 16:48:47 +0200

    profile dynamic data

--- cinematch-frontend/src/models/types/components/user/user.model.ts
+++ cinematch-frontend/src/models/types/components/user/user.model.ts
@@ -1,11 +1,20 @@
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-    bio: string;
-    avatarUrl?: string;
-    birthDate?: string;
-    location?: string;
-    favoriteGenres?: string[];
+
+export class User {
+  username: string;
+  name: string;
+  bio?: string;
+  picture?: string;
+  watchedMoviesCount: number;
+  reviewsCount: number;
+  watchlistCount: number;
+
+  constructor(data?: Partial<User>) {
+    this.username = data?.username || '';
+    this.name = data?.name || '';
+    this.bio = data?.bio || '';
+    this.picture = data?.picture || 'assets/default-avatar.png';
+    this.watchedMoviesCount = data?.watchedMoviesCount || 0;
+    this.reviewsCount = data?.reviewsCount || 0;
+    this.watchlistCount = data?.watchlistCount || 0;
   }
-  
+}
