837018c1fc9a121746896036e8a7a52191002e2e
OumaimaRTAYLI <oumaima.rtayli@gmail.com>
2025-04-17 15:56:11 +0200

    terms + privacy

---
+++ cinematch-frontend/src/app/term-service/term-service.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-term-service',
+  imports: [TranslatePipe],
+  templateUrl: './term-service.component.html',
+  styleUrl: './term-service.component.scss'
+})
+export class TermServiceComponent {
+
+}
