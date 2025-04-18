65a88a8ed7166fce87e3dd2676e4b0f63ef334d1
T-H-O-M-A-S <marin.f.thomas@gmail.com>
2025-04-18 11:38:18 +0200

    feat: IHM form user config

--- cinematch-frontend/src/app/settings/settings.component.ts
+++ cinematch-frontend/src/app/settings/settings.component.ts
@@ -2,19 +2,30 @@ import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { MatButtonModule } from '@angular/material/button';
 import { LanguageService } from '../../services/language/language.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-settings',
-  imports: [TranslatePipe, MatButtonModule],
+  imports: [TranslatePipe, MatButtonModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+
+  selectedLang = '';
+
   constructor(public langService: LanguageService) {}
 
-  changeLanguage() {
-    const nextLang = this.langService.currentLang === 'fr' ? 'en' : 'fr';
-    this.langService.setLanguage(nextLang);
+  changeLanguage($event: MatSelectChange) {
+    console.log("selected lang: ", $event);
+    if ($event?.value) {
+      this.selectedLang = $event.value  
+      this.langService.setLanguage(this.selectedLang);
+    }
+  }
+
+  save() {
   }
 
 }
