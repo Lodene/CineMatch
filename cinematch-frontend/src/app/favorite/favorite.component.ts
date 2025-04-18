2f29d831ae1997e0f9c8f0eb276a213b238e5171
T-H-O-M-A-S <marin.f.thomas@gmail.com>
2025-04-18 08:52:03 +0200

    feat: remove favorite movie from favorite page

--- cinematch-frontend/src/app/favorite/favorite.component.ts
+++ cinematch-frontend/src/app/favorite/favorite.component.ts
@@ -39,4 +39,8 @@ export class FavoriteComponent implements OnInit {
     })
   }
 
+  deleteMovie($event: number): void {
+    this.lovedMovies = this.lovedMovies.filter(el => el.movie.id !== $event);
+  }
+
 }
