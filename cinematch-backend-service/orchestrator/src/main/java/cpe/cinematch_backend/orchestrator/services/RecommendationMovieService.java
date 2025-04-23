6671f7566f03de7e76b86136ca6d457fbe06f9ae
T-H-O-M-A-S <marin.f.thomas@gmail.com>
2025-04-23 17:57:48 +0200

    feat: write & read from activeMQ

--- cinematch-backend-service/orchestrator/src/main/java/cpe/cinematch_backend/orchestrator/services/RecommendationMovieService.java
+++ cinematch-backend-service/orchestrator/src/main/java/cpe/cinematch_backend/orchestrator/services/RecommendationMovieService.java
@@ -1,10 +1,12 @@
 package cpe.cinematch_backend.orchestrator.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.projet_asi_ii.MessageRequest;
 import cpe.cinematch_backend.orchestrator.entities.MovieRecommendationEntity;
 import cpe.cinematch_backend.orchestrator.repositories.MovieRecommendationRepository;
+import cpe.cinematch_backend.orchestrator.requests.MovieRecommendationActiveMqRequest;
 import cpe.cinematch_backend.orchestrator.requests.MovieRecommendationRequest;
 import cpe.cinematch_backend.orchestrator.responses.MovieRecommendationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +15,14 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
-public class SimilarMovieService {
+public class RecommendationMovieService {
 
     @Autowired
     MovieRecommendationRepository movieRecommendationRepository;
@@ -50,33 +53,51 @@ public class SimilarMovieService {
         movieRecommendationEntity.setRecommendationsId(new ArrayList<>());
         movieRecommendationRepository.save(movieRecommendationEntity);
 
+        MovieRecommendationActiveMqRequest movieRequest = new MovieRecommendationActiveMqRequest();
+        movieRequest.setRequestId(uuid.toString());
+        movieRequest.setRecentlyLikedMovies(movieRecommendationRequest.getRecentlyLikedMovies());
+        movieRequest.setFromUsername(movieRecommendationRequest.getFromUsername());
 
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            String jsonObject = objectMapper.writeValueAsString(movieRecommendationRequest);
+
+            // ObjectMapper mapper = new ObjectMapper();
+            // String jsonObject = objectMapper.writeValueAsString(movieRequest);
+
             MessageRequest message = new MessageRequest(uuid.toString(), serviceId, Map.of(
-                    "movie" , jsonObject));
+                    "payload" , movieRequest));
             String jsonMessage = objectMapper.writeValueAsString(message); // on convertit l'objet Java en JSON
             jmsTemplate.convertAndSend(serviceId, jsonMessage); // on envoie du JSON texte, pas un objet
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
-    @JmsListener(destination = "recommendation-movie-response.queue")
-    public void receiveRequest(MovieRecommendationResponse movieRecommendationResponse){
+
+
+    @JmsListener(destination = "recommendation-movie-response.queue", containerFactory = "myFactory")
+    public void receiveRequest(byte[] movieRecommendationResponseJson){
         String serviceId = env.getProperty("activemq.recommendation-movie-response.queue");
         String serviceName = env.getProperty("activemq.recommendation-movie.service");
         if (serviceName == null || serviceId == null) {
             throw new RuntimeException("Error: serviceName or serviceId is null");
         }
+        MovieRecommendationResponse response;
+        try {
+            String json = new String(movieRecommendationResponseJson, StandardCharsets.UTF_8);  // ✅ decode byte[] properly
+            ObjectMapper objectMapper = new ObjectMapper();
+            response = objectMapper.readValue(json, MovieRecommendationResponse.class);
+            System.out.println("✅ Got: " + response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
         Optional<MovieRecommendationEntity> similarMovieRequest = movieRecommendationRepository.findById(movieRecommendationResponse.getRequestId());
         if (similarMovieRequest.isPresent()) {
             // save in db for rollback purpose
-            similarMovieRequest.get().setRecommendationsId(movieRecommendationResponse.getRecommendationsId());
+            similarMovieRequest.get().setRecommendationsId(response.getRecommendationsId());
             movieRecommendationRepository.save(similarMovieRequest.get());
-
+            
             // todo: make an api call to the node js to notify the frontend
         }
-    }
 
+    }
 }
