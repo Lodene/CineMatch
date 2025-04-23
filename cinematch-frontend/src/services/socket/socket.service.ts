30f180f30ce86126c097d5bac538df485b3e53ae
Thomas0507 <marin.f.thomas@gmail.com>
2025-04-23 22:55:17 +0200

    feat: socket-services & finalize movie recommendation endpoint

---
+++ cinematch-frontend/src/services/socket/socket.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+
+  private socket: Socket;
+  private readonly uri: string = 'http://express-socket:3000'; // Your backend URL
+
+  constructor() {
+    this.socket = io(this.uri);
+  }
+  emit(eventName: string, data?: any): void {
+    this.socket.emit(eventName, data);
+  }
+  on<T>(eventName: string): Observable<T> {
+    return new Observable<T>((subscriber) => {
+      this.socket.on(eventName, (data: T) => {
+        subscriber.next(data);
+      });
+    });
+  }
+  disconnect(): void {
+    if (this.socket) {
+      this.socket.disconnect();
+    }
+  }
+}
