0b0575bec64bd15f76f48d06d69615322ac61abd
Chatelet Ferrety Matias <matias.chatelet-ferrety@etu.uni-lyon1.fr>
2025-04-24 16:54:37 +0200

    feat: Display cast in movie details

--- cinematch-frontend/src/app/person-card/person-card.component.ts
+++ cinematch-frontend/src/app/person-card/person-card.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Person } from '../../models/types/components/person/person.model';
+import { MatIconModule } from '@angular/material/icon';  // Importation du module MatIcon
 
 @Component({
   selector: 'app-person-card',
-  imports: [],
+  imports: [MatIconModule],  // Ajoutez ici MatIconModule
   templateUrl: './person-card.component.html',
   styleUrl: './person-card.component.scss'
 })
 export class PersonCardComponent {
   @Input() person: Person;
-
 }
