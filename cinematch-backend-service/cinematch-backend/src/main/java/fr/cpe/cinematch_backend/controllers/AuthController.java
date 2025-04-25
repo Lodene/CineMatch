84364d81d72f99a81d054079bda4b3716eb53a7f
Antoine <aa@isdea.fr>
2025-04-25 10:52:12 +0200

    feat: add first connexion

--- cinematch-backend-service/cinematch-backend/src/main/java/fr/cpe/cinematch_backend/controllers/AuthController.java
+++ cinematch-backend-service/cinematch-backend/src/main/java/fr/cpe/cinematch_backend/controllers/AuthController.java
@@ -5,6 +5,7 @@ import fr.cpe.cinematch_backend.dtos.requests.UserRequest;
 import fr.cpe.cinematch_backend.entities.AppUser;
 import fr.cpe.cinematch_backend.exceptions.BadEndpointException;
 import fr.cpe.cinematch_backend.exceptions.GenericNotFoundException;
+import fr.cpe.cinematch_backend.repositories.AppUserRepository;
 import fr.cpe.cinematch_backend.services.AppUserService;
 import fr.cpe.cinematch_backend.services.UserConfigurationService;
 import fr.cpe.cinematch_backend.services.UserValidateService;
@@ -12,7 +13,9 @@ import fr.cpe.cinematch_backend.services.security.AuthenticationService;
 import fr.cpe.cinematch_backend.services.security.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
@@ -31,6 +34,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("auth")
 public class AuthController {
@@ -42,6 +47,9 @@ public class AuthController {
 
     @Autowired
     private AuthenticationService authenticationService;
+
+    @Autowired
+    private AppUserRepository appUserRepository;
 
     @Autowired
     private AppUserService userService;
@@ -64,11 +72,16 @@ public class AuthController {
         securityContextHolderStrategy.setContext(context);
         securityContextRepository.saveContext(context, request, response);
         String jwtToken = jwtService.generateToken((UserDetails) authenticatedUser.getPrincipal());
+
+        AppUser uE = (AppUser) authenticatedUser.getPrincipal();
+        System.out.println("User: " + uE.getUsername() + " and first connexion : " + uE.isFirstConnexion());
+
         LoginResponse loginResponse = new LoginResponse();
         loginResponse.setToken(jwtToken);
         loginResponse.setExpiresIn(jwtService.getExpirationTime());
         loginResponse.setLang(userConfigurationService.getUserLanguage(userRequest.getUsername()));
         loginResponse.setUsername(userRequest.getUsername());
+        loginResponse.setFirstConnexion(uE.isFirstConnexion());
         return ResponseEntity.ok(loginResponse);
     }
 
@@ -96,6 +109,24 @@ public class AuthController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         AppUser user = (AppUser) authentication.getPrincipal();
         authenticationService.updatePassword(user.getUsername(), password);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/first-connexion/disable")
+    public ResponseEntity<Void> disableFirstConnexion() throws GenericNotFoundException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        AppUser user = (AppUser) authentication.getPrincipal();
+
+        AppUser appUser;
+        appUser = appUserRepository.findByUsername(user.getUsername())
+                .orElseThrow(() -> new GenericNotFoundException(404, "User not found",
+                        "username '" + user.getUsername() + "' does not exist"));
+        if (!user.isFirstConnexion()) {
+            return ResponseEntity.badRequest().build(); // on ne peut pas repasser à true
+        }
+
+        user.setFirstConnexion(false);
+        appUserRepository.save(user);
         return ResponseEntity.ok().build();
     }
 }
